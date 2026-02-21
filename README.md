9d9d9696e6a8714621839de1add3347d451bfdc4
Ernest V <ernest.viol@gmail.com>
2026-02-20 19:33:39 -0800

    Add deployment instructions
    Added deployment instructions and eslint plugins for dev.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Webpack template
 
 A simple webpack template made by Ernest Viola to quickly start new projects.
+
+## Deployment
+
+[Odin Project Webpack Deployment](https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page)
+
+### Steps
+
+1. git branch gh-pages
+2. git checkout gh-pages && git merge main --no-edit
+3. npm run build
+4. npm run deploy
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import jestPlugin from "eslint-plugin-jest";
+import globals from "globals";
 
 export default defineConfig([
   {
@@ -16,7 +18,16 @@ export default defineConfig([
     languageOptions: {
       globals: {
         console: "readonly",
+        ...globals.browser,
+        ...globals.node,
       },
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    ...jestPlugin.configs["flat/recommended"],
+    rules: {
+      ...jestPlugin.configs["flat/recommended"].rules,
     },
   },
 ]);
